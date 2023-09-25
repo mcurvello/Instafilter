feb004e1504ed42c5432245ee3cb5608bfed9851
Marcio Curvello <marciocurvello@gmail.com>
2023-09-24 21:46:52 -0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Marcio Curvello on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
